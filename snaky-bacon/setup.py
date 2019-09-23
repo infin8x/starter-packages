da444dccbd7ed8a2aed7d92f3f373955c3f3e1d4
Alex Mullans <alex.mullans@microsoft.com>
2019-09-23 10:50:11 -0700

    Update

--- snaky-bacon/setup.py
+++ snaky-bacon/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='snaky-bacon',
       description='Python-flavored bacon',
-      version='1.1.1',
+      version='1.1.2',
       py_modules=['snaky-bacon'],
       )
